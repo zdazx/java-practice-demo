d5f7f527d90e56da241996f924d7f9b41dffb3f2
lei.wang1 <lei.wang1@leiwang1s-Mac-mini.local>
2021-01-14 14:07:33 +0800

    add runnable task

--- src/main/java/com/tw/zd/task/RunnableTask.java
+++ src/main/java/com/tw/zd/task/RunnableTask.java
@@ -1,7 +1,21 @@
 package com.tw.zd.task;
 
-public class RunnableTask implements Runnable {
-    public void run() {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class RunnableTask implements Runnable {
+    private final Logger logger = LoggerFactory.getLogger(RunnableTask.class);
+
+    public void run() {
+        logger.info("this is the runnable task");
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(new RunnableTask());
+        executorService.shutdown();
     }
 }
