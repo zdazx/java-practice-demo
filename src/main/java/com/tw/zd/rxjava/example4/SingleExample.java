d9cb26b25eaee5e67c33b19d4be518190a1b15d4
lei.wang1 <lei.wang1@leiwang1s-Mac-mini.local>
2021-01-18 10:55:02 +0800

    add class of creating observable with single method

---
+++ src/main/java/com/tw/zd/rxjava/example4/SingleExample.java
@@ -0,0 +1,26 @@
+package com.tw.zd.rxjava.example4;
+
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+
+public class SingleExample {
+    public static void main(String[] args) {
+        Single.just(1)
+                .subscribe(new SingleObserver<Integer>() {
+                    public void onSubscribe(@NonNull Disposable disposable) {
+
+                    }
+
+                    public void onSuccess(@NonNull Integer integer) {
+                        System.out.println(integer);
+                    }
+
+                    public void onError(@NonNull Throwable throwable) {
+
+                    }
+                });
+    }
+
+}
