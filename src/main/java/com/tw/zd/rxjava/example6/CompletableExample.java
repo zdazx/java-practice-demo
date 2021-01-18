f064b5bb437b11267f2ee1ed75330bdc0493897e
lei.wang1 <lei.wang1@leiwang1s-Mac-mini.local>
2021-01-18 11:02:07 +0800

    add class of creating observable with completable method

---
+++ src/main/java/com/tw/zd/rxjava/example6/CompletableExample.java
@@ -0,0 +1,31 @@
+package com.tw.zd.rxjava.example6;
+
+import io.reactivex.Completable;
+import io.reactivex.CompletableEmitter;
+import io.reactivex.CompletableObserver;
+import io.reactivex.CompletableOnSubscribe;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+
+public class CompletableExample {
+    public static void main(String[] args) {
+        Completable.create(new CompletableOnSubscribe() {
+            public void subscribe(@NonNull CompletableEmitter completableEmitter) {
+                completableEmitter.onComplete();
+            }
+        }).subscribe(new CompletableObserver() {
+            public void onSubscribe(@NonNull Disposable disposable) {
+
+            }
+
+            public void onComplete() {
+                System.out.println("complete");
+            }
+
+            public void onError(@NonNull Throwable throwable) {
+
+            }
+        });
+    }
+
+}
