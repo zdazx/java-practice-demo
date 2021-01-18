59ab6853ec99ffc798e97238add9671e2a776c66
lei.wang1 <lei.wang1@leiwang1s-Mac-mini.local>
2021-01-18 10:53:53 +0800

    dd class of creating observable with from method

---
+++ src/main/java/com/tw/zd/rxjava/example3/FromExample.java
@@ -0,0 +1,34 @@
+package com.tw.zd.rxjava.example3;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FromExample {
+    public static void main(String[] args) {
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4, null, 5);
+        Observable.fromIterable(integers)
+                .subscribe(new Observer<Integer>() {
+                    public void onSubscribe(@NonNull Disposable disposable) {
+                        System.out.println("start");
+                    }
+
+                    public void onNext(@NonNull Integer integer) {
+                        System.out.println(integer);
+                    }
+
+                    public void onError(@NonNull Throwable throwable) {
+                        System.out.println("error");
+                    }
+
+                    public void onComplete() {
+                        System.out.println("complete");
+                    }
+                });
+    }
+
+}
