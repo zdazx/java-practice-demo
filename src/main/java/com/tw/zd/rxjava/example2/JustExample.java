4be5ff9a0955c4fe02de66d614878fd758b6efaa
lei.wang1 <lei.wang1@leiwang1s-Mac-mini.local>
2021-01-18 10:52:01 +0800

    add class of creating observable with just method

---
+++ src/main/java/com/tw/zd/rxjava/example2/JustExample.java
@@ -0,0 +1,31 @@
+package com.tw.zd.rxjava.example2;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+
+public class JustExample {
+    public static void main(String[] args) {
+        Observable.just(1, 2, 3, 4)
+                .subscribe(new Observer<Integer>() {
+                    public void onSubscribe(@NonNull Disposable disposable) {
+                        System.out.println("start");
+                    }
+
+                    public void onNext(@NonNull Integer integer) {
+                        System.out.println("This is " + integer);
+                    }
+
+                    public void onError(@NonNull Throwable throwable) {
+                        System.out.println("error");
+                    }
+
+                    public void onComplete() {
+                        System.out.println("complete");
+                    }
+                });
+
+    }
+
+}
