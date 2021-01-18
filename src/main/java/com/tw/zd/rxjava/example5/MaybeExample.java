443d1732b7a0511e860b3b2b2ada986a44bf1943
lei.wang1 <lei.wang1@leiwang1s-Mac-mini.local>
2021-01-18 11:01:02 +0800

    add class of creating observable with maybe method

---
+++ src/main/java/com/tw/zd/rxjava/example5/MaybeExample.java
@@ -0,0 +1,57 @@
+package com.tw.zd.rxjava.example5;
+
+import io.reactivex.Maybe;
+import io.reactivex.MaybeObserver;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+
+public class MaybeExample {
+    public static void main(String[] args) {
+        getObservableWithMaybe();
+        getObservableWithEmptyMaybe();
+    }
+
+    private static void getObservableWithEmptyMaybe() {
+        Maybe.empty()
+                .subscribe(new MaybeObserver<Object>() {
+                    public void onSubscribe(@NonNull Disposable disposable) {
+                        System.out.println("empty-maybe-start");
+                    }
+
+                    public void onSuccess(@NonNull Object o) {
+                        System.out.println("***" + o);
+                    }
+
+                    public void onError(@NonNull Throwable throwable) {
+
+                    }
+
+                    public void onComplete() {
+                        System.out.println("empty-maybe-complete");
+                    }
+                });
+    }
+
+    private static void getObservableWithMaybe() {
+        Maybe.just(1)
+                .subscribe(new MaybeObserver<Integer>() {
+                    public void onSubscribe(@NonNull Disposable disposable) {
+                        System.out.println("maybe-start");
+                    }
+
+                    public void onSuccess(@NonNull Integer integer) {
+                        System.out.println(integer);
+                    }
+
+                    public void onError(@NonNull Throwable throwable) {
+
+                    }
+
+                    public void onComplete() {
+                        System.out.println("maybe-complete");
+                    }
+                });
+
+    }
+
+}
