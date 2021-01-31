1265da8f016bafc060889f42cf2cafbb8e552fec
zdazx <zhangduo0227@163.com>
2021-01-31 20:10:50 +0800

    add test of rate limiter using tryAcquire() that is not blocking.

--- src/test/java/com/tw/zd/guava/GuavaTest.java
+++ src/test/java/com/tw/zd/guava/GuavaTest.java
@@ -4,8 +4,10 @@ import com.google.common.util.concurrent.RateLimiter;
 import org.junit.jupiter.api.Test;
 
 import java.time.ZonedDateTime;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GuavaTest {
@@ -21,5 +23,17 @@ public class GuavaTest {
         int elapsedTime = ZonedDateTime.now().getSecond() - startTime;
 
         assertTrue(elapsedTime >= 10);
+    }
+
+    @Test
+    void should_get_rate_limiter_resource_without_blocking() {
+        RateLimiter rateLimiter = RateLimiter.create(1);
+
+        rateLimiter.acquire();
+        boolean result = rateLimiter.tryAcquire(2, 10, TimeUnit.MILLISECONDS);
+
+        assertFalse(result);
+
+
     }
 }
